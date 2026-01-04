272412a2df0734dcfbf3273a0e2c32428838abf2
JoeAitkenDev <josephaitken21@gmail.com>
2026-01-04 11:25:40 +0000

    Initialized API to retrieve recipe data. Also added template of the 'media card' to the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,10 +12,17 @@ import {
   IonGrid,
   IonRow,
   IonSearchbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
 } from '@ionic/angular/standalone';
 
 import { addIcons } from 'ionicons';
 import { cogSharp, heartSharp, searchSharp, trashBin } from 'ionicons/icons';
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -34,15 +41,33 @@ import { cogSharp, heartSharp, searchSharp, trashBin } from 'ionicons/icons';
     IonRow,
     IonSearchbar,
     RouterLink,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
   ],
 })
 export class HomePage {
-  constructor() {
+  options: HttpOptions = {
+    url: this.mhs.getRecipesURL,
+  };
+
+  constructor(private mhs: MyHttpService) {
     addIcons({
       heartSharp,
       cogSharp,
       trashBin,
       searchSharp,
     });
+  }
+
+  ngOnInit() {
+    this.getRecipes();
+  }
+
+  async getRecipes() {
+    let result = await this.mhs.get(this.options);
+    console.log(result);
   }
 }
---
+++ src/app/services/my-http.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorHttp, HttpOptions } from '@capacitor/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MyHttpService {
+  // Store the API key to a variable
+  private apiKey = '70759a4f7911402abcc53d3c51d3b759';
+
+  // Append the apikey to the url to grant access for get requests
+  public getRecipesURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${this.apiKey}`;
+
+  constructor() {}
+
+  async get(options: HttpOptions) {
+    return await CapacitorHttp.get(options);
+  }
+}
